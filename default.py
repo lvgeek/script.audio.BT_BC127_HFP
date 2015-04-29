98a7e66686659193ce79e74fa509892e2dec1305
dave <lvgeek101@gmail.com>
2015-04-29 16:15:21 -0500

    modified:   default.py

--- default.py
+++ default.py
@@ -1,45 +1,46 @@
-# Import necessary modules
+# -*- coding: utf-8 -*-
+# Licence: GPL v.3 http://www.gnu.org/licenses/gpl.html
+# This is an XBMC addon using PyXBMCt framework.
+# XBMC connection to BC127 Bluetooth interface 
+# Sparkfun Product: https://www.sparkfun.com/products/11924
+# lvgeek: https://github.com/lvgeek
+
+import os
 import xbmc
+import xbmcgui
+import xbmcaddon
 import pyxbmct.addonwindow as pyxbmct
 
-# Create a class for our UI
-class MyAddon(pyxbmct.AddonDialogWindow):
 
+class MyAddon(pyxbmct.AddonFullWindow):
     def __init__(self, title=''):
         """Class constructor"""
-        # Call the base class' constructor.
         super(MyAddon, self).__init__(title)
-        # Set width, height and the grid parameters
-        self.setGeometry(300, 230, 4, 2)
-        # Call set controls method
+        screenx = self.getWidth()
+        screeny = self.getHeight()
+        self.setGeometry(screenx, screeny, 5, 9)
         self.set_controls()
-        # Call set navigation method.
         self.set_navigation()
-        # Connect Backspace button to close our addon.
         self.connect(pyxbmct.ACTION_NAV_BACK, self.close)
 
     def set_controls(self):
         """Set up UI controls"""
-        # Image control
-        image = pyxbmct.Image('xbmc-logo.png')
-        self.placeControl(image, 0, 0, rowspan=2, columnspan=2)
-        # Text label
-        label = pyxbmct.Label('Your name:')
-        self.placeControl(label, 2, 0)
-        # Text edit control
-        self.name_field = pyxbmct.Edit('')
-        self.placeControl(self.name_field, 2, 1)
-        # Close button
-        self.close_button = pyxbmct.Button('Close')
-        self.placeControl(self.close_button, 3, 0)
-        # Connect close button
-        self.connect(self.close_button, self.close)
-        # Hello button.
-        self.hello_buton = pyxbmct.Button('Hello')
-        self.placeControl(self.hello_buton, 3, 1)
-        # Connect Hello button.
-        self.connect(self.hello_buton, lambda:
-            xbmc.executebuiltin('Notification(Hello {0}!, Welcome to PyXBMCt.)'.format(self.name_field.getText())))
+        self.1_button = pyxbmct.Button('1')
+        self.placeControl(self.1_button, 0, 0)
+
+#        self.2_button = pyxbmct.Button('2')
+#        self.placeControl(self.1_button, 0, 2)
+#        self.3_button = pyxbmct.Button('3')
+#        self.placeControl(self.1_button, 0, 3)
+#        self.4_button = pyxbmct.Button('4')
+#        self.placeControl(self.1_button, 1, 0)
+#        self.close_button = pyxbmct.Button('Close')
+#        self.placeControl(self.close_button, 4, 0)
+#        self.connect(self.close_button, self.close)
+#        self.hello_buton = pyxbmct.Button('Hello')
+#        self.placeControl(self.hello_buton, 3, 1)
+#        self.connect(self.hello_buton, lambda:
+#            xbmc.executebuiltin('Notification(Hello {0}!, Welcome to PyXBMCt.)'.format(self.name_field.getText())))
 
     def set_navigation(self):
         """Set up keyboard/remote navigation between controls."""
@@ -48,10 +49,9 @@ class MyAddon(pyxbmct.AddonDialogWindow):
         self.close_button.controlLeft(self.hello_buton)
         self.close_button.controlRight(self.hello_buton)
         self.hello_buton.setNavigation(self.name_field, self.name_field, self.close_button, self.close_button)
-        # Set initial focus.
         self.setFocus(self.name_field)
 
 if __name__ == '__main__':
-    myaddon = MyAddon('PyXBMCt Example')
+    myaddon = MyAddon('Bluetooth Phone Interface')
     myaddon.doModal()
     del myaddon
